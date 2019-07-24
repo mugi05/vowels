1f2a3722b9c9526c428bce6766b60ae3c42c8110
mugi05 <53095150+mugi05@users.noreply.github.com>
2019-07-24 20:34:50 +0530

    Add files via upload

---
+++ vowels.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Mugesh05
+#
+# Created:     23/07/2019
+# Copyright:   (c) Mugesh05 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+
+x=input("enter the chr")
+m=str('x')
+m=('a' or 'e' or 'i' or 'o' or 'u'   )
+if(x==m):
+    print("vowels")
+else:
+    print("consonant")
+
+
